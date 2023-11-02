6751e60cd5cb1a99d5aad5eeb0f0a164a6b7ddcd
liasovIhsan04 <liasovihsan1@gmail.com>
2023-11-02 18:41:33 +0600

    ihsam

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ const App = () => {
         <div>
           <Header/>
         </div>
-        
     );
 };
 
---
+++ src/pages/Promotion.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Promotion = () => {
+  return (
+    <div className="promotion">
+      <div className="container"></div>
+    </div>
+  );
+};
+
+export default Promotion;
