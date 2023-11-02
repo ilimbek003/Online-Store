d22ac1ec0fa67748470813360c1fd65760c1e7ef
liasovIhsan04 <liasovihsan1@gmail.com>
2023-11-02 19:25:46 +0600

    ih

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
+import { Routes, Route } from "react-router-dom";
+import Promotion from "./pages/Promotion";
 const App = () => {
-    return (
-        <div>
-            <Header/>
-            <Main/>
-            <Footer/>
-        </div>
-    );
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="promotion" element={<Promotion/>}/>
+      </Routes>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
--- src/pages/Promotion.jsx
+++ src/pages/Promotion.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { LiaQuestionCircleSolid } from "react-icons/lia";
+import GetShot from "../components/GetShot";
+import bekbekei from "../../src/img/2.png";
 
 const Promotion = () => {
   return (
     <div className="promotion">
-      <div className="container"></div>
+      <div className="nav d-flex justify-content-between align-items-center">
+        <span></span>
+        <img className="images" src={bekbekei} alt="Бекбекей" />
+        <LiaQuestionCircleSolid className="fi" />
+      </div>
     </div>
   );
 };
