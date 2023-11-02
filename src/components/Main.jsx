1c5016d684271edc92b03f4a48c9a983750000e2
ilimbek003 <kaiyrbekuulualumbek@gmail.com>
2023-11-02 19:39:40 +0600

    fds

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,19 +3,21 @@ import "../style/css/main.css";
 import "../style/css/App.css";
 import qar from "../img/qr-код.gif";
 import Header from "./Header";
+import MyCoupon from "./MyCoupon";
 
 const Main = () => {
-  return (
-    <>
-      <Header />
-      <div className="container">
-        <div className="bonus_block_all">
-          <h1 className="title_h1">26 бонус</h1>
-          <img src={qar} alt="" />
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Header/>
+            <div className="container">
+                <div className="bonus_block_all">
+                    <h1 className="title_h1">26 бонус</h1>
+                    <img src={qar} alt=""/>
+                </div>
+            </div>
+            <MyCoupon/>
+        </>
+    );
 };
 
 export default Main;
--- src/components/MyCoupon.jsx
+++ src/components/MyCoupon.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import "./MyCoupon.css";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import burger from "../img/burger.jpg";
 import or_cod from "../img/qr-код.gif";
 import { useNavigate } from "react-router";
 import shoro from "../img/shoro.jpg";
+import '../style/css/main.css'
 
 const coupon = [
     {
@@ -37,31 +37,32 @@ const MyCoupon = () => {
                         Бардыгы <MdOutlineKeyboardArrowRight size={18} />
                     </h4>
                 </div>
-                <div className="coupon_block_all">
-                    {coupon.map((el, id) => (
-                        <div
-                            key={id}
-                            className="coupon_box"
-                            onClick={() => navigate(`/my-coupon-details-id/${el.id}`)}
-                        >
-                            <div className="title_goods">
-                                <img src={el.image} alt="" />
-                            </div>
-                            <div className="text_box">
-                                <h3>{el.skidka}</h3>
-                                <div className="or_cod_little_block">
-                                    <p>{el.date}</p>
-                                    <div className="or_cod_little">
-                                        <img src={el.qr_cod} alt="" />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                </div>
             </div>
         </div>
     );
 };
 
 export default MyCoupon;
+
+// <div className="coupon_block_all">
+//     {coupon.map((el, id) => (
+//         <div
+//             key={id}
+//             className="coupon_box"
+//             onClick={() => navigate(`/my-coupon-details-id/${el.id}`)}
+//         >
+//             <div className="title_goods">
+//                 <img src={el.image} alt="" />
+//             </div>
+//             <div className="text_box">
+//                 <h3>{el.skidka}</h3>
+//                 <div className="or_cod_little_block">
+//                     <p>{el.date}</p>
+//                     <div className="or_cod_little">
+//                         <img src={el.qr_cod} alt="" />
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     ))}
+// </div>
