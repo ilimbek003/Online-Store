abcff77ae8b821530ad713825586b18a9e0074e1
ilimbek003 <kaiyrbekuulualumbek@gmail.com>
2023-11-02 18:30:09 +0600

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {FiUser} from "react-icons/fi"
+import {BiBell} from "react-icons/bi"
+import bekbekei from "../img/2.png"
+import '../style/css/App.css'
+const Header = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="nav d-flex justify-content-between align-items-center">
+                     <FiUser className="fi"/>
+                    <img className="images" src={bekbekei} alt="Бекбекей"/>
+                    <BiBell className="fi"/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
