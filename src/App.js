e7841b11e343e08a6af3396784d13d2683d44282
liasovIhsan04 <liasovihsan1@gmail.com>
2023-11-02 19:45:40 +0600

    ihsan

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Main from "./components/Main";
 import Footer from "./components/Footer";
 import { Routes, Route } from "react-router-dom";
 import Promotion from "./pages/Promotion";
+import GetShot from "./components/GetShot";
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="promotion" element={<Promotion/>}/>
+        <Route path="/promotion" element={<Promotion/>}/>
       </Routes>
       <Footer />
     </div>
--- src/pages/Promotion.jsx
+++ src/pages/Promotion.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { LiaQuestionCircleSolid } from "react-icons/lia";
-import GetShot from "../components/GetShot";
 import bekbekei from "../../src/img/2.png";
+import GetShot from "../components/GetShot";
 
 const Promotion = () => {
   return (
@@ -11,6 +11,7 @@ const Promotion = () => {
         <img className="images" src={bekbekei} alt="Бекбекей" />
         <LiaQuestionCircleSolid className="fi" />
       </div>
+      <GetShot/>
     </div>
   );
 };
