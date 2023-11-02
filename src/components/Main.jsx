8e93cff4cf84d0b1cfdcc23a79a07d3910574ab7
ilimbek003 <kaiyrbekuulualumbek@gmail.com>
2023-11-02 18:41:55 +0600

    ds

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div className="container">
+            <div className="bonus_block_all">
+
+            </div>
+        </div>
+    );
+};
+
+export default Main;
