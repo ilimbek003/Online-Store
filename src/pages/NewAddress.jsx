4d4ff3dd345556725b69740843491bce207690fc
liasovIhsan04 <liasovihsan1@gmail.com>
2023-11-05 05:06:18 +0600

    ihsan

--- src/pages/NewAddress.jsx
+++ src/pages/NewAddress.jsx
@@ -14,10 +14,43 @@ const NewAddress = () => {
         </div>
       </div>
       <div className="container">
-        <div className="new_address_block">
-          <label></label>
-          <input type="text" />
-        </div>
+        <form className="input_block new_block">
+          <div className="input_box">
+            <label>
+              Город <span>*</span>
+            </label>
+            <input className="input_form new_add_input" type="text" />
+          </div>
+          <div className="input_box">
+            <label>
+              Улица <span>*</span>{" "}
+            </label>
+            <input className="input_form new_add_input" type="text" />
+          </div>
+          <div className="input_box">
+            <label>
+              Дом <span>*</span>
+            </label>
+            <input className="input_form new_add_input" type="text" />
+          </div>
+          <div className="input_box">
+            <label>Корпус</label>
+            <input className="input_form new_add_input" type="text" />
+          </div>
+          <div className="input_box">
+            <label>Подьезд</label>
+            <input className="input_form new_add_input" type="text" />
+          </div>
+          <div className="input_box">
+            <label>Этаж</label>
+            <input className="input_form new_add_input" type="text" />
+          </div>
+          <div className="input_box">
+            <label>Квартира</label>
+            <input className="input_form new_add_input" type="text" />
+          </div>
+          <button className="new_button">Сохранить</button>
+        </form>
       </div>
     </div>
   );
