68c920c541dfcabc19f2d594676dd749ff801d95
ilimbek003 <kaiyrbekuulualumbek@gmail.com>
2023-11-08 05:51:42 +0600

    ds

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,31 +1,33 @@
 import React from 'react';
-import { AiOutlineHome } from 'react-icons/ai';
+import {AiOutlineHome} from 'react-icons/ai';
 import {BsShop} from "react-icons/bs"
 import {LuBadgePercent} from 'react-icons/lu'
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import '../style/css/main.css'
+import qrCode from "../img/qrcode-svgrepo-com.svg"
+
 const Footer = () => {
     return (
         <div id="footer">
-            <div className="navs">
+            <div className="nav_footer">
                 <div className="flex_block">
                     <NavLink exact to="/" activeClassName="active">
-                        <AiOutlineHome className="fi ai" />
+                        <AiOutlineHome className="fi ai"/>
                         <span>Башкы</span>
                     </NavLink>
                 </div>
                 <div className="flex_block">
                     <NavLink to="/shop-all/shop" activeClassName="active">
-                        <BsShop className="fi ai" />
+                        <BsShop className="fi ai"/>
                         <span>Дyкон</span>
                     </NavLink>
                 </div>
                 <NavLink to="/qr-cod">
-                    <div className="logo"></div>
+                    <img className="footer_qr_code" src={qrCode} alt=""/>
                 </NavLink>
                 <div className="flex_block">
                     <NavLink to="/promotion" activeClassName="active">
-                        <LuBadgePercent className="fi ai" />
+                        <LuBadgePercent className="fi ai"/>
                         <span>Акциялар</span>
                     </NavLink>
                 </div>
