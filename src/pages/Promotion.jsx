119d51f55da852155989d5df46743c74f50fe1cd
liasovIhsan04 <liasovihsan1@gmail.com>
2023-11-09 20:01:13 +0600

    ihsan

--- src/pages/Promotion.jsx
+++ src/pages/Promotion.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { LiaQuestionCircleSolid } from "react-icons/lia";
 import bekbekei from "../../src/img/2.png";
 import GetShot from "../components/GetShot";
 import SpecialOffers from "../components/SpecialOffers";
 import { useNavigate } from "react-router";
+import axios from "axios";
 
 const Promotion = () => {
   const navigate = useNavigate();
+  const [buy, setBuy] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`/product/card-type/`)
+      .then((response) => {
+        setBuy(response.buy);
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+      });
+  }, []);
+
   return (
     <div className="promotion">
       <div className="nav">
