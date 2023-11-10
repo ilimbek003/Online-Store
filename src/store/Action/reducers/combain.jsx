e29cf0743436acdaa1f44c98a9c5620e204edfda
liasovIhsan04 <liasovihsan1@gmail.com>
2023-11-10 08:23:07 +0600

    ihsan

---
+++ src/store/Action/reducers/combain.jsx
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+import postReducer from "./reducersRegister";
+
+export const rootReducer = combineReducers({
+  post: postReducer,
+  login: LoginReducer,
+  post_forgot: postForgot,
+  code: codeReducer,
+});
