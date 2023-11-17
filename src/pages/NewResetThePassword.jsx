21b80d33f1070f406ce288d1e1d7300c8a8bcbe1
liasovIhsan04 <liasovihsan1@gmail.com>
2023-11-18 00:27:18 +0600

    dfdfd

--- src/pages/NewResetThePassword.jsx
+++ src/pages/NewResetThePassword.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { HiArrowLongLeft } from "react-icons/hi2";
 import { useNavigate } from "react-router";
 import { FaEye } from "react-icons/fa";
@@ -20,9 +20,21 @@ const NewResetThePassword = ({ Alert }) => {
   const [error, setError] = useState([]);
   const [password, setPassword] = useState("");
   const [confirm_password, setConfirmPassword] = useState("");
+  const [local, setLocal] = useState("");
   const dispatch = useDispatch();
-
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setLocal(token);
+    }
+    console.log(token)
+  }, []);
+
+  const headers = {
+    Authorization: `Token ${local}`,
+  };
 
   const handleNewSumbit = async (event) => {
     event.preventDefault();
@@ -35,7 +47,8 @@ const NewResetThePassword = ({ Alert }) => {
     try {
       const response = await axios.post(
         url + "/auth/change-password",
-        newPasswordCredential
+        newPasswordCredential,
+        { headers: headers }
       );
       dispatch(registerSuccess(response.data));
       if (response.data.response == true) {
