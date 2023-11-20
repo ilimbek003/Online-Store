9a4edd8e2ad69391c7ad5739c30e128078026f1c
liasovIhsan04 <liasovihsan1@gmail.com>
2023-11-20 19:54:39 +0600

    ihsan

--- src/pages/MyInformation.jsx
+++ src/pages/MyInformation.jsx
@@ -4,8 +4,10 @@ import { useNavigate } from "react-router";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { TbFileSettings } from "react-icons/tb";
 import IMask from "imask";
+import axios from "axios";
+import { url } from "../Api";
 
-const data = [
+const dataSelect = [
   {
     name: "1",
   },
@@ -24,8 +26,24 @@ const data = [
 ];
 
 const MyInformation = () => {
+  const [data, setData] = useState([]);
   const [info, setInfo] = useState({
     family: "",
+    phone: "",
+    first_name: "",
+    last_name: "",
+    bonus_id: "",
+    bonus: null,
+    qrimg: "https://bekbekei.store/media/user/bonus-qr/312996777777777.png",
+    birthday: "2023-11-20",
+    gender: "",
+    language: "",
+    married: "",
+    status: "",
+    city: "",
+    children: true,
+    animal: true,
+    car: true,
   });
   useEffect(() => {
     const phoneInput = document.getElementById("phone");
@@ -35,6 +53,15 @@ const MyInformation = () => {
       });
     }
   });
+
+  useEffect(() => {
+    axios
+      .get(url + "/auth/user-info")
+      .then((response) => setData(response.data))
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
 
   const navigate = useNavigate();
 
@@ -49,106 +76,116 @@ const MyInformation = () => {
       </div>
       <div className="container">
         <form>
-          <div className="input_form_block top">
-            <div className="input_box">
-              <label>Авторизация телефону</label>
-              <input
-                id="phone"
-                className="input_form new_add_input"
-                placeholder="996"
-              />
-            </div>
-            <div className="input_box">
-              <label>Фaмилиясы</label>
-              <input className="input_form new_add_input" type="text" />
-            </div>
-            <div className="input_box">
-              <label>Аты</label>
-              <input className="input_form new_add_input" type="text" />
-            </div>
-            <div className="input_box">
-              <label>Атасынын Аты</label>
-              <input className="input_form new_add_input" type="text" />
-            </div>
-            <div className="input_box ">
-              <label>Туулган куну</label>
-              <input className="input_form new_add_input" type="date" />
-            </div>
-            <div className="input_box ">
-              <label>Жынысы</label>
-              <input className="input_form new_add_input" type="text" />
-            </div>
-            <div className="input_box">
-              <label>Тили</label>
-              <select
-                className="input_select "
-                onChange={(e) => setInfo({ ...info, family: e.target.value })}
-                value={info.family}
-              >
-                {data.map((el, id) => (
-                  <option key={id} value={el.name}>
-                    {el.name}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className="input_box">
-              <label>Уй-булолук абалы</label>
-              <select
-                className="input_select "
-                onChange={(e) => setInfo({ ...info, family: e.target.value })}
-                value={info.family}
-              >
-                {data.map((el, id) => (
-                  <option key={id} value={el.name}>
-                    {el.name}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className="input_box">
-              <label>Социалдык абалы</label>
-              <select
-                className="input_select "
-                onChange={(e) => setInfo({ ...info, family: e.target.value })}
-                value={info.family}
-              >
-                {data.map((el, id) => (
-                  <option key={id} value={el.name}>
-                    {el.name}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className="input_box">
-              <label>Жашаган шаары</label>
-              <select
-                className="input_select "
-                onChange={(e) => setInfo({ ...info, family: e.target.value })}
-                value={info.family}
-              >
-                {data.map((el, id) => (
-                  <option key={id} value={el.name}>
-                    {el.name}
-                  </option>
-                ))}
-              </select>
-            </div>
-          </div>
-          <div className="toggle_block toogle_block_inform">
-            <p>Уй-жаныбарларынын болушу</p>
-            <label className="switch">
-              <input type="checkbox" />
-              <span className="slider_toggle round"></span>
-            </label>
-          </div>
-          <div className="toggle_block toogle_block_inform bottom_tom">
-            <p>Унаанын болушу</p>
-            <label className="switch">
-              <input type="checkbox" />
-              <span className="slider_toggle round"></span>
-            </label>
-          </div>
+            <>
+              <div className="input_form_block top">
+                <div className="input_box">
+                  <label>Авторизация телефону</label>
+                  <input
+                    id="phone"
+                    className="input_form new_add_input"
+                    placeholder="996"
+                  />
+                </div>
+                <div className="input_box">
+                  <label>Фaмилиясы</label>
+                  <input className="input_form new_add_input" type="text" />
+                </div>
+                <div className="input_box">
+                  <label>Аты</label>
+                  <input className="input_form new_add_input" type="text" />
+                </div>
+                <div className="input_box">
+                  <label>Атасынын Аты</label>
+                  <input className="input_form new_add_input" type="text" />
+                </div>
+                <div className="input_box ">
+                  <label>Туулган куну</label>
+                  <input className="input_form new_add_input" type="date" />
+                </div>
+                <div className="input_box ">
+                  <label>Жынысы</label>
+                  <input className="input_form new_add_input" type="text" />
+                </div>
+                <div className="input_box">
+                  <label>Тили</label>
+                  <select
+                    className="input_select "
+                    onChange={(e) =>
+                      setInfo({ ...info, family: e.target.value })
+                    }
+                    value={info.family}
+                  >
+                    {dataSelect.map((el, id) => (
+                      <option key={id} value={el.name}>
+                        {el.name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div className="input_box">
+                  <label>Уй-булолук абалы</label>
+                  <select
+                    className="input_select "
+                    onChange={(e) =>
+                      setInfo({ ...info, family: e.target.value })
+                    }
+                    value={info.family}
+                  >
+                    {dataSelect.map((el, id) => (
+                      <option key={id} value={el.name}>
+                        {el.name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div className="input_box">
+                  <label>Социалдык абалы</label>
+                  <select
+                    className="input_select "
+                    onChange={(e) =>
+                      setInfo({ ...info, family: e.target.value })
+                    }
+                    value={info.family}
+                  >
+                    {data.map((el, id) => (
+                      <option key={id} value={el.name}>
+                        {el.name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div className="input_box">
+                  <label>Жашаган шаары</label>
+                  <select
+                    className="input_select "
+                    onChange={(e) =>
+                      setInfo({ ...info, family: e.target.value })
+                    }
+                    value={info.family}
+                  >
+                    {data.map((el, id) => (
+                      <option key={id} value={el.name}>
+                        {el.name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </div>
+              <div className="toggle_block toogle_block_inform">
+                <p>Уй-жаныбарларынын болушу</p>
+                <label className="switch">
+                  <input type="checkbox" />
+                  <span className="slider_toggle round"></span>
+                </label>
+              </div>
+              <div className="toggle_block toogle_block_inform bottom_tom">
+                <p>Унаанын болушу</p>
+                <label className="switch">
+                  <input type="checkbox" />
+                  <span className="slider_toggle round"></span>
+                </label>
+              </div>
+            </>
         </form>
       </div>
     </div>
@@ -156,53 +193,3 @@ const MyInformation = () => {
 };
 
 export default MyInformation;
-
-//    <div className="input_box">
-// <div
-// onClick={() =>
-//   setToursTwo(!toursTwo) ||
-//   setToursThree(false) ||
-//   setToursFour(false) ||
-//   setToursFife(false) ||
-//   setTours(false) ||
-//   setChekbox(false)
-// }
-// className="select_input"
-// ></div>
-// <input
-// value={dataTours.transport}
-// disabled={true}
-// onChange={(e) =>
-//   setDataTours({ ...dataTours, transport: e.target.value })
-// }
-// type="text"
-// placeholder={t("key17")}
-// className="category_box"
-// />
-// <RiArrowDownSLine
-// onClick={() => setToursTwo(!toursTwo)}
-// className="icon"
-// size={25}
-// />
-// {toursTwo && (
-// <Select>
-//   {list.transport &&
-//     list.transport &&
-//     list.transport.map((el) => (
-//       <div
-//         key={el.id}
-//         onClick={() => {
-//           setDataTours({
-//             ...dataTours,
-//             transport: el.name,
-//           });
-//           setToursTwo(false);
-//         }}
-//         className="select_box"
-//       >
-//         {el.name}
-//       </div>
-//     ))}
-// </Select>
-// )}
-// </div>
