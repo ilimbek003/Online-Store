f359e2714353f9e51d9c4fee1f0f1c1649a1f641
liasovIhsan04 <liasovihsan1@gmail.com>
2023-12-02 21:18:14 +0600

    ihsan

--- src/pages/GetShotDetails.jsx
+++ src/pages/GetShotDetails.jsx
@@ -1,55 +1,54 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import "../style/css/main.css";
-import {useNavigate} from "react-router";
-import {HiArrowLongLeft} from "react-icons/hi2";
+import { useNavigate } from "react-router";
+import { HiArrowLongLeft } from "react-icons/hi2";
 import promotionkg from "../../src/img/promotionkg.jpg";
 import kola from "../../src/img/kola.jpg";
 import manty from "../../src/img/manty.jpg";
 import globus from "../../src/img/globus.jpg";
 import hleb from "../../src/img/hleb.jpg";
-import {useDispatch, useSelector} from "react-redux";
-import {fetchData} from "../Redux/reduser/fetchData";
-
+import { useDispatch, useSelector } from "react-redux";
+import { fetchData } from "../Redux/reduser/fetchData";
 
 const GetShotDetails = () => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const {data} = useSelector(state => state.myData);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { data } = useSelector((state) => state.myData);
 
-    useEffect(() => {
-        dispatch(fetchData());
-    }, [dispatch]);
-    return (
-        <div id="modal">
-            <div className="nav">
-                <div className="container d-flex justify-content-between align-items-center ">
-                    <HiArrowLongLeft
-                        className="fi"
-                        style={{cursor: "pointer"}}
-                        onClick={() => navigate(-1)}
-                        size={20}
-                    />
-                    <p className="header_name">Все акции</p>
-                    <span></span>
-                </div>
-            </div>
-            <div className="get_shot_details top">
-                <div className="container">
-                    <div className="get_details_block_all top_all">
-                        {data.map((el, id) => (
-                            <div
-                                key={id}
-                                onClick={() => navigate(`/get-shot-details-id/${el.id}`)}
-                                className="special_details_box"
-                            >
-                                <img className="special_image" src={el.img} alt=""/>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
+  return (
+    <div id="modal">
+      <div className="nav">
+        <div className="container d-flex justify-content-between align-items-center ">
+          <HiArrowLongLeft
+            className="fi"
+            style={{ cursor: "pointer" }}
+            onClick={() => navigate(-1)}
+            size={20}
+          />
+          <p className="header_name">Все акции</p>
+          <span></span>
         </div>
-    );
+      </div>
+      <div className="get_shot_details top">
+        <div className="container">
+          <div className="get_details_block_all top_all">
+            {data.map((el, id) => (
+              <div
+                key={id}
+                onClick={() => navigate(`/get-shot-details-id/${el.id}`)}
+                className="special_details_box"
+              >
+                <img className="special_image" src={el.img} alt="" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default GetShotDetails;
--- src/pages/Promotion.jsx
+++ src/pages/Promotion.jsx
@@ -1,29 +1,28 @@
-import React, {useEffect, useState} from "react";
-import {LiaQuestionCircleSolid} from "react-icons/lia";
+import React, { useEffect, useState } from "react";
+import { LiaQuestionCircleSolid } from "react-icons/lia";
 import bekbekei from "../../src/img/bekbekei-h-logo.svg";
 import GetShot from "../components/GetShot";
 import SpecialOffers from "../components/SpecialOffers";
-import {useNavigate} from "react-router";
-
+import { useNavigate } from "react-router";
 
 const Promotion = () => {
-    const navigate = useNavigate();
-    return (
-        <div className="promotion">
-            <div className="nav">
-                <div className="container d-flex justify-content-between align-items-center ">
-                    <span></span>
-                    <img className="images" src={bekbekei} alt="Бекбекей"/>
-                    <LiaQuestionCircleSolid
-                        className="fi"
-                        onClick={() => navigate("/to-help-page")}
-                    />
-                </div>
-            </div>
-            <GetShot/>
-            <SpecialOffers/>
+  const navigate = useNavigate();
+  return (
+    <div className="promotion">
+      <div className="nav">
+        <div className="container d-flex justify-content-between align-items-center ">
+          <span></span>
+          <img className="images" src={bekbekei} alt="Бекбекей" />
+          <LiaQuestionCircleSolid
+            className="fi"
+            onClick={() => navigate("/to-help-page")}
+          />
         </div>
-    );
+      </div>
+      <GetShot />
+      <SpecialOffers />
+    </div>
+  );
 };
 
 export default Promotion;
